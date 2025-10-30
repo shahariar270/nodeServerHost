785d998b102db9033a499ae4f08a7ce4f077fc95
shahariar270 <mdshahariar102@gmail.com>
2025-10-30 07:14:44 +0600

    Update server configuration for environment compatibility and code formatting

--- server.js
+++ server.js
@@ -2,33 +2,33 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
+const hostname = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
     let filePath = ''
-    console.log(__dirname);
-    if(req.url === '/'){
+
+    if (req.url === '/') {
         filePath = path.join(__dirname, 'public', 'index.html');
-    } else if(req.url === '/about'){
+    } else if (req.url === '/about') {
         filePath = path.join(__dirname, 'public', 'about.html');
-    } else if(req.url === '/contact'){
+    } else if (req.url === '/contact') {
         filePath = path.join(__dirname, 'public', 'contact.html');
     }
 
     fs.readFile(filePath, (err, data) => {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.writeHead(404, { 'Content-Type': 'text/html' });
             res.end('<h1>404 Not Found</h1>');
         } else {
             console.log(data);
-            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(data);
         }
     });
 
 });
 
-server.listen(port, hostname, ()=>{
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 })
